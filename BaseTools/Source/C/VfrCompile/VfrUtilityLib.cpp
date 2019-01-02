e5ea4bc316d0fea9fc58bbefc3a4846d0f25c2e4
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2019-01-02 11:07:59 +0200

    Update VfrUtilityLib.cpp
    Fix compile with any version of gcc

--- BaseTools/Source/C/VfrCompile/VfrUtilityLib.cpp
+++ BaseTools/Source/C/VfrCompile/VfrUtilityLib.cpp
@@ -3280,7 +3280,7 @@ CVfrStringDB::GetVarStoreNameFormStringId (
   UINT8       BlockType;
   EFI_HII_STRING_PACKAGE_HDR *PkgHeader;
   
-  if (mStringFileName == '\0' ) {
+  if (mStringFileName[0] == '\0' ) {
     return NULL;
   }
 
